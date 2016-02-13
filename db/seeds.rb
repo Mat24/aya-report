3ad610ac7c53ffe031f9a8fa420fe8371c78c190
Mat24 <smat_24@hotmail.com>
2016-02-13 16:10:24 -0500

    full devise integration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(email: "admin@cooperas.com", password: "lol12345")
+User.create(email: "admin@cooperas.com.co", password: "lol12345")
